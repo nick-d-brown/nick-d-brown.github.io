02afae4497b83fbeb07cf105ca1d3de0d7ff7ba8
Nick Brown <n.brown.professional@gmail.com>
2018-05-23 01:36:15 -0600

    adds total portfolio redesign

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bootstrap-Portfolio
-This is portfolio was designed using the Twitter Bootstrap framework. It implements both the predesigned classes and layout grid system included with Bootstrap. 
---
+++ assets/logic.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    // global variables
+    // var learnFlag = false;
+
+    //initializes materialize functionality
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+
+   
+   // creates appear/disappear effect on classes
+    window.sr = ScrollReveal({reset:true});
+    sr.reveal('.section',
+        { 
+            duration: 500,
+            origin: 'right',
+            // , rotate: { x: 360, y: 360, z: 360 }
+            mobile: true,
+            // , viewFactor: 0.7
+            duration: 1000,
+            delay: 100,
+
+         }
+    );
+
+    
+        $(document).scroll(function () {
+            var nav = $("#navigationBar");
+            var para = $(".parallax-container")
+            nav.toggleClass('fixedLoc', $(this).scrollTop() > (para.height()));
+            para.toggleClass('paraBottomFix', $(this).scrollTop() > (para.height()));
+            
+        });
+    
+    $(".learn-more-button").on("click", function () {
+        // $("#moreAbout").toggleClass("hidden-html");
+        // if (learnFlag === false) {
+        //     $(".learn-more-button").text("Learn Less");
+        //     learnFlag = true;
+        // } else {
+        //     $(".learn-more-button").text("Learn More"); 
+        //     learnFlag = false;
+        // }
+        $("#moreAbout").fadeToggle("slow");
+    })
+
+ 
+
+});
+
